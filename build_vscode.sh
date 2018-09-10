feeb1e22e68c9adbc69245a4cacc54296acd303b
Christian Weichel <christian.weichel@typefox.io>
2018-09-10 10:25:19 +0000

    Run up to date scripts

--- build_vscode.sh
+++ build_vscode.sh
@@ -4,6 +4,4 @@ cd /tmp
 git clone https://github.com/microsoft/vscode
 cd vscode
 yarn
-
-export DISPLAY=$((cd /tmp/.X11-unix && for x in X*; do echo ":${x#X}"; done) | head -n 1 | tr '\n' ' ')
 ./scripts/code.sh
