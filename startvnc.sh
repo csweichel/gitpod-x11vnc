17c233e6eb06d9665fb758930d9b0311dfc170a0
Christian Weichel <christian.weichel@typefox.io>
2018-09-10 10:35:27 +0000

    Better startvncs.sh

--- startvnc.sh
+++ startvnc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd $(mktemp -d)
+pwd
 git clone https://github.com/novnc/noVNC.git novnc
+echo "<html><head><meta http-equiv=\"Refresh\" content=\"5; url=vnc_lite.html\"></head></html>" > novnc/index.html
 cd novnc/utils && ./launch.sh --vnc localhost:5900 &
 x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -create -forever
