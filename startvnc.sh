17d529e72df7355502f2c5af21e5fc1ea435a0a3
Christian Weichel <christian.weichel@typefox.io>
2018-09-10 11:23:19 +0200

    Added more required deps

--- startvnc.sh
+++ startvnc.sh
@@ -3,4 +3,4 @@
 cd $(mktemp -d)
 git clone https://github.com/novnc/noVNC.git novnc
 cd novnc/utils && ./launch.sh --vnc localhost:5900 &
-x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -nocache 10 -create
+x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -create
