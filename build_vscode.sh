8bb9aafb65be1c0de9d07ba247501b08d7b848c4
Christian Weichel <christian.weichel@typefox.io>
2018-09-10 12:12:15 +0200

    now with better display detection

--- build_vscode.sh
+++ build_vscode.sh
@@ -4,4 +4,6 @@ cd /tmp
 git clone https://github.com/microsoft/vscode
 cd vscode
 yarn
-DISPLAY=:0 ./scripts/code.sh
+
+export DISPLAY=$((cd /tmp/.X11-unix && for x in X*; do echo ":${x#X}"; done) | head -n 1 | tr '\n' ' ')
+./scripts/code.sh
